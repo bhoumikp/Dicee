f6ce4ae33e4b9f4a503ede76d8a14286db9cf760
Bhoumik Pagdhare <61223883+bhoumikp@users.noreply.github.com>
2020-09-18 15:53:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ document.querySelectorAll("img")[1].setAttribute("src", "dice" + player2 + ".png
 
 // if player 1 wins
 if (player1 > player2) {
-	document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+	document.querySelector("h1").textContent = "Player 1 Wins!";
 } 
 // if player 2 wins
 else if (player1 < player2) {
-	document.querySelector("h1").textContent = "🚩 Player 2 Wins!";
+	document.querySelector("h1").textContent = "Player 2 Wins!";
 } 
 // if both players wins
 else {
