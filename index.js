9cc70a1a1b0cf80a7a7ebf1e2df4814d54cfd8a5
Bhoumik Pagdhare <61223883+bhoumikp@users.noreply.github.com>
2020-11-11 20:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const player1Name = prompt('Player 1');
 const player2Name = prompt('Player 2');
 
-
 document.querySelector("#player1").textContent = player1Name;
 document.querySelector("#player2").textContent = player2Name;
 
 
 // Event Listeners
-document.addEventListener("click", rolldice);
-document.addEventListener("keydown", function(event) {
-	if (event.key == " ") {
-		rolldice();
-	}
-})
+if (player1Name && player2Name != null) {
+	document.addEventListener("click", rolldice);
+	document.addEventListener("keydown", function(event) {
+		if (event.key == " ") {
+			rolldice();
+		}
+	})
+} else {
+	alert('Please Enter Player Names!');
+	location.reload();
+}
 
 
 function rolldice() {
-
 	// Player 2 dice
 	var player1 = Math.floor(Math.random() * 6 + 1);
 	document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + player1 + ".png");
